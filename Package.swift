43fe4efb6cc88545ee97509a213a899caef926fe
Darren Ehlers <me@darrenehlers.com>
2022-07-27 14:58:50 -0500

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -26,12 +26,12 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DoubleNode/DNSAppCore.git", from: "1.9.1"),
-        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.9.8"),
-        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.4"),
+        .package(url: "https://github.com/DoubleNode/DNSAppCore.git", from: "1.9.2"),
+        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.9.9"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.7"),
         .package(url: "https://github.com/DoubleNode/DNSCrashWorkers.git", from: "1.9.8"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.8.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.9.29"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.9.30"),
         .package(url: "https://github.com/nidegen/Geodesy", from: "1.2.2"),
         .package(url: "https://github.com/Square/Valet", from: "4.1.2"),
     ],
